7173b7f8dbc8295876cf1ef0ed6f1c9a40f5d3f5
D Tucker <danielle@tuckerlabs.com>
2013-08-24 10:39:39 -0700

    Angular js on Rails Part 1 Initial setup and testing of models

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AngularCasts::Application.config.secret_key_base = 'acc1547144fd58e31a8a29a195004767c7471dd6c4027fb18a6ae9c570cb6fb3d909e17340425e5045504066f198a148cf0d1798598aed696a4bf92145badf65'
